89bb8e84f8845190daf7a630481e4d80b4e89d59
Qazi <qazi.husnain878@gmail.com>
2023-06-15 05:21:24 -0700

    impliment button and link files

--- gui.py
+++ gui.py
@@ -1,9 +1,22 @@
 import functions
 import PySimpleGUI as sg
 label=sg.Text("Enter a Todo in list")
-input_text=sg.InputText(tooltip="enter any thing")
+input_text=sg.InputText(tooltip="please enter a todo",key="todo")
 add_btn=sg.Button("ADD")
-window=sg.Window("QAZI's Todo APP",layout=[[label,input_text,add_btn]])
-window.read()
+window=sg.Window("QAZI's Todo APP",
+                 layout=[[label],[input_text,add_btn]],
+                 font=('Helvetica',12))
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "ADD":
+            todolist=functions.get_todolist()
+            new_todo=values['todo'] + '\n'
+            todolist.append(new_todo)
+            functions.write_todolist(todolist)
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
 
