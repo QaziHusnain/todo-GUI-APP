13e2b90d500d0c0434d1542d04c1ab760db5363a
Qazi <qazi.husnain878@gmail.com>
2023-06-16 02:09:48 -0700

    romve bug in edit and add function and complete and exit function added complete application

--- gui.py
+++ gui.py
@@ -6,37 +6,77 @@ add_btn=sg.Button("ADD")
 list_box=sg.Listbox(values=functions.get_todolist(),
                     key='todos',enable_events=True,size=[45,10])
 edit_btn=sg.Button("EDIT")
+complete_btn=sg.Button("COMPLETE")
+exit_btn=sg.Button("EXIT")
 window=sg.Window("QAZI's Todo APP",
-                 layout=[[label],[input_text,add_btn],[list_box,edit_btn]],
+                 layout=[[label],[input_text,add_btn],[list_box,edit_btn,complete_btn],[exit_btn]],
                  font=('Helvetica',12))
 while True:
     event,values=window.read()
-    print(event)
-    print(values)
+    #print(event)
+
 
 
     match event:
         case "ADD":
+
             todolist = functions.get_todolist()
             new_todo = values['todo']+ "\n"
-            todolist.append(new_todo)
-            functions.write_todolist(todolist)
-            window['todos'].update(values=todolist)
+            blank_string=new_todo.strip()
+            blank_check=len(blank_string)
+
+            if blank_check>0:
+
+                todolist.append(new_todo)
+                functions.write_todolist(todolist)
+                window['todos'].update(values=todolist)
+            else:
+                sg.popup("please first enter then add")
         case "EDIT":
+            try:
 
-            todo_to_edit=values['todos'][0]
-            new_todo=values['todo']
+                todo_to_edit=values['todos'][0]
+                new_todo=values['todo']+'\n'
+                blank_string=new_todo.strip()
+                blank_check=len(blank_string)
+                print=len(values['todo'])
 
 
-            todolist=functions.get_todolist()
-            index_to_edit=todolist.index(todo_to_edit)
+                if blank_check>0 and new_todo.strip()!=todo_to_edit.strip():
 
-            todolist[index_to_edit] = new_todo
-            functions.write_todolist(todolist)
-            window['todos'].update(values=todolist)
+
+
+                    todolist=functions.get_todolist()
+                    index_to_edit=todolist.index(todo_to_edit)
+
+                    todolist[index_to_edit] = new_todo
+                    functions.write_todolist(todolist)
+                    window['todos'].update(values=todolist)
+                else:
+                    sg.popup("Please first enter then press EDIT",font=['Helvetica',14])
+            except IndexError:
+                sg.popup("Please select first any item to edit",font=['Helvetica',14])
+
         case "todos":
 
-            window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0].strip())
+        case "COMPLETE":
+            try:
+                todo_to_remove = values['todos'][0]
+                todolist = functions.get_todolist()
+
+                todolist.remove(todo_to_remove)
+                functions.write_todolist(todolist)
+                window['todos'].update(values=todolist)
+                window['todo'].update(value="")
+
+            except IndexError:
+                sg.popup("Please select first any item to Remove", font=['Helvetica', 14])
+        case "EXIT":
+            break
+
+
+
 
 
         case sg.WINDOW_CLOSED:
